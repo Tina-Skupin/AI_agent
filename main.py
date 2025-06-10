0beb4948e9f364c6075eafeeaec653f23c8a2b51
Tina-Skupin <tskupin32@gmail.com>
2025-06-10 13:29:20 +0200

    agent command line

--- main.py
+++ main.py
@@ -7,17 +7,31 @@ api_key = os.environ.get("GEMINI_API_KEY")
 
 from google import genai
 
-client = genai.Client(api_key=api_key)
+
+from google.genai import types
+
 if len(sys.argv) == 1:
     print ("Error")
     sys.exit(1)
 
+messages = [
+    types.Content(role="user", parts=[types.Part(text=sys.argv[1])]),
+]
+
+client = genai.Client(api_key=api_key)
+
+
+
 response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents=sys.argv[1]
+    model="gemini-2.0-flash-001",
+    contents=messages,
 )
 
-
-print(response.text)
-print ("Prompt tokens:", response.usage_metadata.prompt_token_count)
-print ("Response tokens:", response.usage_metadata.candidates_token_count)
-print (str(sys.argv))
+if len(sys.argv) == 3:
+    if sys.argv[2] == "--verbose":
+        print ("User prompt:", str(sys.argv))
+        print ("Prompt tokens:", response.usage_metadata.prompt_token_count)
+        print ("Response tokens:", response.usage_metadata.candidates_token_count)
+        print(response.text)
+else:
+    print(response.text)
